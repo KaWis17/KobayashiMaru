95436a9296e096f5f14e7e37a451fc4143951afb
Krzysztof Antoni Wiśniewski <krzymado@gmail.com>
2024-07-17 02:10:48 +0200

    Added bibliografia and dodatekA

--- PythonScripts/boardToSVG/main.py
+++ PythonScripts/boardToSVG/main.py
@@ -1,7 +1,10 @@
 import chess.svg
 
 board = chess.Board(chess.STARTING_BOARD_FEN)
+board.push_uci('e2e4')
 svg_text = chess.svg.board(board, size=900)
+
+print(board.fen())
 
 with open('boardToSVG/out/board.svg', 'w') as f:
     f.write(svg_text)
